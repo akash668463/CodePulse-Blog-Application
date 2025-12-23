faccce1cd02dfae075851cb6c51efd2b86ebd2f7
Akash <ak668463@gmail.com>
2025-12-24 02:27:52 +0530

    adding add-category component and using Reactive forms

---
+++ UI/CodePulse/src/app/features/category/add-category/add-category.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-category',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-category.html',
+  styleUrl: './add-category.css',
+})
+export class AddCategory {
+  //1. Import ReactiveFormsModule
+  //2. FormGroups -> FormControls
+
+  addCategoryFormGroup = new FormGroup({
+    name : new FormControl<string>('', {nonNullable: true}),
+    urlHandle : new FormControl<string>('', {nonNullable: true}),
+  });
+
+  onSubmit(){
+    console.log(this.addCategoryFormGroup.getRawValue())
+  }
+}
