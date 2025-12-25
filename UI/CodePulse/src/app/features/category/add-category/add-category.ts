c1489f40785a00a07a16383a19e31f15c24ac664
Akash <ak668463@gmail.com>
2025-12-25 22:13:13 +0530

    Creating Angular Service and making HTTP Request

--- UI/CodePulse/src/app/features/category/add-category/add-category.ts
+++ UI/CodePulse/src/app/features/category/add-category/add-category.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AddCategoryRequest } from '../models/category.model';
+import { CategoryService } from '../services/category-service';
 
 @Component({
   selector: 'app-add-category',
@@ -8,6 +10,19 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './add-category.css',
 })
 export class AddCategory {
+  constructor(){
+    effect(()=>{
+      if(this.categoryService.addCategoryStatus() === 'success'){
+        console.log('Success');
+        //Redirect back to category list page
+      }
+      if(this.categoryService.addCategoryStatus() === 'error'){
+        console.log('Add category Request Failed');
+        //Redirect back to category list page
+      }     
+    });
+  }
+  private categoryService = inject(CategoryService)
   //1. Import ReactiveFormsModule
   //2. FormGroups -> FormControls
 
@@ -30,6 +45,17 @@ export class AddCategory {
   }
 
   onSubmit(){
-    console.log(this.addCategoryFormGroup.getRawValue())
+    const addCategoryFormValue = this.addCategoryFormGroup.getRawValue();
+
+    const AddCategoryRequestDto : AddCategoryRequest = {
+      name : addCategoryFormValue.name,
+      urlHandle : addCategoryFormValue.urlHandle,
+    };
+
+    this.categoryService.addCategory(AddCategoryRequestDto);
+
+    
   }
+
+  
 }
