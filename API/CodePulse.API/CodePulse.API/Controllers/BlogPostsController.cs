9de0970137732602e924493d3ff1ee55eabb5319
Akash <ak668463@gmail.com>
2026-01-18 22:14:41 +0530

    Creating  BlogPostsController and adding endpoint CreateBlogPost

---
+++ API/CodePulse.API/CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,53 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+        //POST: {apibaseurl}/api/BlogPosts
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto requestDto)
+        {
+            //convert DTO to domain model
+            var blogPost = new BlogPost
+            {
+                Title = requestDto.Title,
+                shortDescription = requestDto.shortDescription,
+                Content = requestDto.Content,
+                FeaturedImageUrl = requestDto.FeaturedImageUrl,
+                UrlHandle = requestDto.UrlHandle,
+                PublishedDate = requestDto.PublishedDate,
+                Author = requestDto.Author,
+                IsVisible = requestDto.IsVisible
+            };
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+            // Convert Domain Model back to DTO
+
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Title = blogPost.Title,
+                Content = blogPost.Content,
+                Author = blogPost.Author,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                shortDescription = blogPost.shortDescription,
+                UrlHandle = blogPost.UrlHandle
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Models/DTO/CreateBlogPostRequestDto.cs
@@ -0,0 +1,20 @@
+﻿namespace CodePulse.API.Models.DTO
+{
+    public class CreateBlogPostRequestDto
+    {
+        public string Title { get; set; }
+
+        public string shortDescription { get; set; }
+
+        public string Content { get; set; }
+
+        public string FeaturedImageUrl { get; set; }
+
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+        public string Author { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
