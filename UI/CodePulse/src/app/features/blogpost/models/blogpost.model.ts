036a1d407274f87df3f233335e65109f12c19ad7
Akash <ak668463@gmail.com>
2026-02-03 00:20:19 +0530

    Get All BlogPost UI Changes

--- UI/CodePulse/src/app/features/blogpost/models/blogpost.model.ts
+++ UI/CodePulse/src/app/features/blogpost/models/blogpost.model.ts
@@ -1,3 +1,5 @@
+import { Category } from "../../category/models/category.model";
+
 export interface AddBlogPostRequest{
     title : string,
     shortDescription : string,
@@ -18,4 +20,5 @@ export interface BlogPost{
     author : string,
     publishedDate : Date,
     isVisible : boolean,
+    categories : Category[]
 }
